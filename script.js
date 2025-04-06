425d99c4d588b799e02c85309658053f131a1f13
md-mudassir7 <mohammedmudassir.soudagar@amagi.com>
2025-04-06 21:53:31 +0530

    add: more changes

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-// script.js
 function showImage(el) {
-    const main = document.getElementById("mainPreview");
-    const thumbnails = document.querySelectorAll(".thumbnail-gallery img");
+    // Find the closest container (section)
+    const container = el.closest(".container");
   
+    // Find the main preview image within this section
+    const main = container.querySelector("img[id^='mainPreview']");
+  
+    // Get thumbnails only inside this section
+    const thumbnails = container.querySelectorAll(".thumbnail-gallery img");
+  
+    // Transition effect
     main.style.opacity = 0;
     setTimeout(() => {
       main.src = el.src;
       main.style.opacity = 1;
     }, 150);
   
+    // Remove active from all, add to clicked
     thumbnails.forEach(img => img.classList.remove("active-thumb"));
     el.classList.add("active-thumb");
   }
-  
   
